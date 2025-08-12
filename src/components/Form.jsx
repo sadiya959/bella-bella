c7a4f4c7390da6257815a1b3b26432aea385ed23
sadiya959 <sacdamamoud@gmail.com>
2025-08-12 15:52:55 +0300

    added confirm message after form submition

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { MdDone } from "react-icons/md";
 
 const preferredTimes = [
   "5:00 PM",
@@ -18,27 +20,46 @@ const numberGuests = [1, 2, 3, 4, 5, 6, 7, "8+"];
 const Form = () => {
   const { register, handleSubmit } = useForm();
 
+  const [confirm, setConfirm] = useState(false);
+
   const onsubmit = (data) => {
     console.log(data);
+
+    setConfirm(true);
+
+    setTimeout(() => {
+      setConfirm(false);
+    }, 3000);
   };
 
-  return (
+  return confirm ? (
+    <div className="mx-auto mt-12 flex max-w-2xl flex-col items-center justify-center gap-4 rounded-lg bg-white px-4 py-12 shadow-lg">
+      <div className="rounded-full bg-green-100 p-6 text-green-800">
+        <MdDone size={20} />
+      </div>
+      <h2 className="text-3xl text-green-600"> Reservation Confirmed!</h2>
+      <p className="text-gray-600">
+        Thank you for choosing Bella Vista. We look forward to serving you.
+      </p>
+    </div>
+  ) : (
     <form
       onSubmit={handleSubmit(onsubmit)}
-      className="bg-white shadow-lg rounded-lg w-full my-12 p-8"
+      className="my-12 w-full rounded-lg bg-white p-8 shadow-lg"
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4 mb-6">
+      <div className="mb-6 grid grid-cols-1 items-center gap-4 md:grid-cols-2">
         {/* input group  */}
         <div className="flex flex-col">
           <label
-            className="text-sm text-gray-700 font-medium mb-1"
+            className="mb-1 text-sm font-medium text-gray-700"
             htmlFor="fullname"
           >
             Full Name *
           </label>
           <input
-            className="border border-gray-200 px-4 py-3 outline-none hover:border-[#d97706] rounded-lg"
+            className="rounded-lg border border-gray-200 px-4 py-3 outline-none hover:border-[#d97706]"
             type="text"
+            required
             placeholder="Enter your fullname"
             {...register("fullname")}
           />
@@ -46,14 +67,15 @@ const Form = () => {
         {/* input group  */}
         <div className="flex flex-col">
           <label
-            className="text-sm text-gray-700 font-medium mb-1"
+            className="mb-1 text-sm font-medium text-gray-700"
             htmlFor="email"
           >
             Email Address *
           </label>
           <input
-            className="border border-gray-200 px-4 py-3 outline-none hover:border-[#d97706] rounded-lg"
+            className="rounded-lg border border-gray-200 px-4 py-3 outline-none hover:border-[#d97706]"
             type="email"
+            required
             placeholder="Enter your email"
             {...register("email")}
           />
@@ -62,14 +84,15 @@ const Form = () => {
         {/* input group  */}
         <div className="flex flex-col">
           <label
-            className="text-sm text-gray-700 font-medium mb-1"
+            className="mb-1 text-sm font-medium text-gray-700"
             htmlFor="phone"
           >
             Phone Number *
           </label>
           <input
-            className="border border-gray-200 px-4 py-3 outline-none hover:border-[#d97706] rounded-lg"
+            className="rounded-lg border border-gray-200 px-4 py-3 outline-none hover:border-[#d97706]"
             type="phone"
+            required
             placeholder="Enter your Phone number"
             {...register("phone")}
           />
@@ -77,20 +100,21 @@ const Form = () => {
         {/* input group  */}
         <div className="flex flex-col">
           <label
-            className="text-sm text-gray-700 font-medium mb-1"
+            className="mb-1 text-sm font-medium text-gray-700"
             htmlFor="date"
           >
             Reservation Date *
           </label>
           <input
-            className="border border-gray-200 px-4 py-3 outline-none hover:border-[#d97706] rounded-lg"
+            className="rounded-lg border border-gray-200 px-4 py-3 outline-none hover:border-[#d97706]"
             type="date"
+            required
             {...register("date")}
           />
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         {/* Preferred Time */}
         <div>
           <label lassName="text-sm text-gray-700 font-medium mb-1">
@@ -104,11 +128,12 @@ const Form = () => {
               >
                 <input
                   type="radio"
+                  required
                   value={preferredTime}
                   {...register("preferredTime")}
-                  className="hidden peer"
+                  className="peer hidden"
                 />
-                <span className="block py-3 px-8 text-center border border-gray-200 rounded-lg shadow bg-white   peer-checked:bg-[#d97706] peer-checked:text-white">
+                <span className="block rounded-lg border border-gray-200 bg-white px-8 py-3 text-center shadow peer-checked:bg-[#d97706] peer-checked:text-white">
                   {preferredTime}
                 </span>
               </label>
@@ -117,10 +142,10 @@ const Form = () => {
         </div>
         {/* Number of Guests */}
         <div>
-          <label className="text-sm text-gray-700 font-medium mb-1">
+          <label className="mb-1 text-sm font-medium text-gray-700">
             Number of Guests *
           </label>
-          <div className="grid grid-cols-4 gap-2 ">
+          <div className="grid grid-cols-4 gap-2">
             {numberGuests.map((numberGuest) => (
               <label
                 key={numberGuest}
@@ -128,11 +153,12 @@ const Form = () => {
               >
                 <input
                   type="radio"
+                  required
                   value={numberGuest}
                   {...register("numberGuest")}
-                  className="hidden peer"
+                  className="peer hidden"
                 />
-                <span className="peer-checked:bg-[#d97706] peer-checked:text-white block py-3 px-8 text-center border border-gray-200 rounded-lg shadow bg-white">
+                <span className="block rounded-lg border border-gray-200 bg-white px-8 py-3 text-center shadow peer-checked:bg-[#d97706] peer-checked:text-white">
                   {numberGuest}
                 </span>
               </label>
@@ -144,23 +170,23 @@ const Form = () => {
       {/* Special Requests */}
       <div className="mt-6">
         <label
-          className="text-sm text-gray-700 w-full font-medium"
+          className="w-full text-sm font-medium text-gray-700"
           htmlFor="request"
         >
           Special Requests *
         </label>
         <textarea
-          className="w-full border border-gray-200 outline-none py-3 px-4 rounded-lg mt-1"
+          className="mt-1 w-full rounded-lg border border-gray-200 px-4 py-3 outline-none"
           name="request"
           {...register("request")}
           placeholder="Any dietary restrictions, allergies, or special occasions we should know about?"
         ></textarea>
-        <p className="text-gray-500 text-sm">0/500 characters</p>
+        <p className="text-sm text-gray-500">0/500 characters</p>
       </div>
 
-      <div className="my-6 flex justify-center items-center">
+      <div className="my-6 flex items-center justify-center">
         <button
-          className="bg-[#d97706] rounded-lg cursor-pointer text-white px-12 py-4 outline-none hover:border-[#d97706] text-lg font-bold"
+          className="cursor-pointer rounded-lg bg-[#d97706] px-12 py-4 text-lg font-bold text-white outline-none hover:border-[#d97706]"
           type="submit"
         >
           Reserve Table
